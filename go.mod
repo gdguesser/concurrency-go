25c51d339bdb801cb6d0172054cf823ae2d35bee
gdguesser <gdguesser@gmail.com>
2022-10-20 22:43:17 +0100

    initial commit, next tests with waitgroups

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gdguesser/concurrency-go
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"tetha",
+		"epsilon",
+	}
+
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
+	printSomething("this is the second thing to be printed!", &wg)
+}
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+
+
+
+
+
+
